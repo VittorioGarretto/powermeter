5a4453da8724f0e7a0c6070de60489b90e8d45b5
Vittorio Garretto <vittorio.garretto@gmail.com>
2025-01-25 17:05:30 +0100

    get power draw from nvidia gpu (don't use default timestamp)

--- nvidia_power.py
+++ nvidia_power.py
@@ -1,17 +1,10 @@
 import subprocess
 import time
 
-CMD_GPU = r"nvidia-smi --query-gpu=power.draw --format=csv,noheader,nounits"
-CMD_TIME = r"nvidia-smi --query-gpu=timestamp --format=csv,noheader,nounits"
+CMD_GPU = r"nvidia-smi -i 0 --query-gpu=power.draw --format=csv,noheader,nounits"
 
 def get_gpu_power():
     output = subprocess.run(CMD_GPU,
-                            shell=True,
-                            stdout=subprocess.PIPE)
-    return output.stdout.decode().rstrip().strip()
-    
-def get_time():
-    output = subprocess.run(CMD_TIME,
                             shell=True,
                             stdout=subprocess.PIPE)
     return output.stdout.decode().rstrip().strip()
@@ -19,14 +12,14 @@ def get_time():
 def print_data():
     output = ""
     output += f"GPU: {get_gpu_power()}\n"
-    output += f"TIME: {get_time()}\n"
+    output += f"TIME: {time.time()}\n"
     return output
 
 def save_data():
-    with open('data/labels.csv', 'a') as file:
+    with open('labels.csv', 'a') as file:
         file.write(get_gpu_power())
-        file.write(", ") 
-        file.write(get_time())
+        file.write(",") 
+        file.write(str(time.time()))
         file.write("\n")
 
 def main():
