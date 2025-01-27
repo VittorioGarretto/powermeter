be63df779aecee005f0b61134573d57c91ed4699
Vittorio Garretto <vittorio.garretto@gmail.com>
2025-01-27 10:38:43 +0100

    Install perf required

--- cpu_power.sh
+++ cpu_power.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 cmd="sudo perf stat -a -e power/energy-pkg/ sleep 0.5 2>&1 | head -4 | tail -1 | awk '{print \$1}'"
 
-export ELEMENTO_POWER_CPU
+export POWER_CPU
 
 joules=$(eval $cmd)
-ELEMENTO_POWER_CPU=$(echo "scale=4; ($joules) / .5" | bc)
+POWER_CPU=$(echo "scale=2; ($joules) / .5" | bc)
 
-echo $ELEMENTO_POWER_CPU
+echo $POWER_CPU
