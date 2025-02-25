0614dc657f1777749587c6a82961160a11c7ea07
Vittorio Garretto <vittorio.garretto@gmail.com>
2025-02-25 20:53:02 +0100

    deleted files

--- cpu_power.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cmd="sudo perf stat -a -e power/energy-pkg/ sleep 0.5 2>&1 | head -4 | tail -1 | awk '{print \$1}'"
-
-export POWER_CPU
-
-joules=$(eval $cmd)
-POWER_CPU=$(echo "scale=2; ($joules) / .5" | bc)
-
-echo $POWER_CPU
